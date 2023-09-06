6dff7654b644301b6a89da759bb9fab6a3897c38
Asha Gaire <asha.gaire95@gmail.com>
2023-09-06 16:37:02 +0300

    few changes in first project

--- reactinfosite/src/App.js
+++ reactinfosite/src/App.js
@@ -1,14 +1,60 @@
 import './App.css';
 
+function Header() {
+  return(
+    <header>
+      <nav className='nav'>
+        <img src={require("./logo192.png" )} className='nav-logo'/>
+        
+        <ul className='nav-items'>
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+
+        
+      </nav>
+    </header>
+  )
+}
+
+function Footer() {
+  return(
+    <footer> @2023 Asha Development. All rights reserved</footer>
+  )
+}
+
+function MainContent() {
+  return(
+    <div className='main-content'>
+      <h1>Fun facts about React</h1>
+          
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by jordan Walke</li>
+        <li>Has well over 100K stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprises apps, including mobile apps</li>
+
+      </ul>
+
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header />
+        <MainContent/>
         
-        <h1>hello</h1>
+        <Footer/>  
+        
       </header>
     </div>
   );
 }
+
 
 export default App;
--- reactinfosite/src/index.js
+++ reactinfosite/src/index.js
@@ -11,9 +11,15 @@ root.render(
   </React.StrictMode>
 );
 
+// const h1= document.createElement("h1")
+// h1.textContent = "some more content"
+// h1.className = "header"
+// document.getElementById("root").append(h1)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
 
 // ReactDOM.createRoot(document.getElementById("root")).render(<h1>hellow</h1>)
+
+
