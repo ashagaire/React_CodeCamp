b278d6e3004395a1b26d09a3076dee92ed71e95e
Asha Gaire <asha.gaire95@gmail.com>
2023-09-07 15:39:29 +0300

    Completed react info site project.

---
+++ reactinfosite/src/components/Main.js
@@ -0,0 +1,18 @@
+export default function Main() {
+    return(
+        
+      <div className='main-content'>
+        <h1 className='main--title'>Fun facts about React</h1>
+            
+        <ul className='list--items'>
+          <li>Was first released in 2013</li>
+          <li>Was originally created by jordan Walke</li>
+          <li>Has well over 100K stars on Github</li>
+          <li>Is maintained by Facebook</li>
+          <li>Powers thousands of enterprises apps, including mobile apps</li>
+  
+        </ul>
+  
+      </div>
+    )
+  }
---
+++ reactinfosite/src/components/Navbar.js
@@ -0,0 +1,17 @@
+export default function Navbar() {
+    return(
+
+       
+     
+        <nav className='nav'>
+          <img src={require("../images/react-icon.png" )} className='nav--logo'/>
+
+          <h3 className="nav--logo_text"> ReactFacts</h3>
+          <h4 className="nav--title">React Course - Projects 1</h4>
+  
+          
+        </nav>
+      
+    )
+  }
+
