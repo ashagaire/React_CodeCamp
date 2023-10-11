60588303f95ca8988f0e545510c444ae48b86445
Asha Gaire <asha.gaire95@gmail.com>
2023-10-11 14:29:02 +0300

    final changes in meme generator app. Added text over image, made api call for data(images) and used  react  useEffect.

--- meme-generator/src/components/Meme.js
+++ meme-generator/src/components/Meme.js
@@ -1,36 +1,83 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import memesData from  "./memesData"
 export default function Meme(){
 
     const [meme ,setMeme] = useState({topText : "" , bottomText : "", randomImage: ""  })
 
-
+    function handleChange(event) {
+        const {name,value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme, [name] : value
+        }))
+    }
     
-    const [allMemeImages, setAllMemeImages]= useState(memesData.data.memes)
-
+    
+    
+    const [allMemes, setAllMemes]= useState([])
     function getMemeImage(){
-        const memesArray= allMemeImages
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
+        
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
         setMeme(preMeme => ({
             ...preMeme,
-            randomImage: url
+            randomImage: url,
+            topText : "" ,
+             bottomText : ""
         }))
         
-    }
         
+    }
+     /**
+    useEffect takes a function as its parameter. If that function
+    returns something, it needs to be a cleanup function. Otherwise,
+    it should return nothing. If we make it an async function, it
+    automatically retuns a promise instead of a function or nothing.
+    Therefore, if you want to use async operations inside of useEffect,
+    you need to define the function separately inside of the callback
+    function, as seen below:
+    */
+    
+    useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+        .then(res => res.json())
+        .then(data => setAllMemes(data.data.memes))
+    }, [])
+
+
+    
 
     
     return(
         <main >
             <div className="form">
                 
-                <input type="Text" placeholder="top-text" className="form-input" />
-                <input type="Text" placeholder="bottom-text" className="form-input"  />
-                <button className="form-button" onClick={getMemeImage}>Get a new meme image  🖼 </button>
+                <input 
+                type="Text" 
+                placeholder="top-text" 
+                name="topText" 
+                value={meme.topText} 
+                onChange={handleChange} 
+                className="form-input" 
+                />
+                <input 
+                type="Text" 
+                placeholder="bottom-text" 
+                name="bottomText" 
+                value={meme.bottomText} 
+                onChange={handleChange} 
+                className="form-input"
+                />
+
+                <button className="form-button" 
+                onClick={getMemeImage}>Get a new meme image  🖼 </button>
                 
             </div>
-            <img src={meme.randomImage} className="meme--image"/>
+            <div className="meme">
+                <img src={meme.randomImage} name="randomImage" className="meme--image" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
+            
         </main>
     )
 }
